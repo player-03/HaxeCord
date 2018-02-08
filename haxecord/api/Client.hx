417171cc94b0b4043df1309af87c5c17225396a6
Joseph Cloutier <jkcloutier@gmail.com>
2018-02-07 20:00:31 -0500

    Enable rate limiting.
    Without this, Client will always busy-wait.

--- haxecord/api/Client.hx
+++ haxecord/api/Client.hx
@@ -50,10 +50,10 @@ class Client implements FutureFactory
 		else return y;
 	}
 
-	public function new(?messageLimit:Int) 
+	public function new(?messageLimit:Int, ?limiter:Float) 
 	{
 		if (messageLimit != null) messageHistoryLimit = min(100, messageLimit);
-		loop = new EventLoop();
+		loop = new EventLoop(limiter);
 		http = new APIHTTP(this, loop);
 	}
 	
